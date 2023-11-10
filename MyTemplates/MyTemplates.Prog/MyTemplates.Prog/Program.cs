5b0d842f01a85dafe341409ae8282e76ee0c1cc7
Ruzil Zakirov <ruzyzakov@gmail.com>
2023-11-10 17:52:58 +0300

    [add] Templates

---
+++ MyTemplates/MyTemplates.Prog/MyTemplates.Prog/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MyTemplates;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Закиров Рузиль";
+        Console.WriteLine(MyTemplateMethods.GreetingMessage(name));
+
+        var deliveryModel = new DeliveryModel { fullname = "Закиров Рузиль", adress = "ДУ 18" };
+        Console.WriteLine(MyTemplateMethods.DeliveryMessage(deliveryModel));
+
+        var videoAccessModel = new VideoAccessModel { fullname = "Закиров Рузиль", age = 17 };
+        Console.WriteLine(MyTemplateMethods.VideoAccessMessage(videoAccessModel));
+
+        var groupModel = new GroupModel
+        {
+            fullname = "Закиров Рузиль",
+            Subject = "Прога",
+            group = "11-209",
+            students = new List<Student> { new Student { Name = "Илья", Surname = "Климкин" }, new Student { Name = "Тимур", Surname = "Минуллин" } }
+        };
+        Console.WriteLine(MyTemplateMethods.GroupInfoMessage(groupModel));
+    }
+}
---
+++ MyTemplates/MyTemplates.Prog/MyTemplates/Template.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyTemplates
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    public class DeliveryModel
+    {
+        public string fullname { get; set; }
+        public string adress { get; set; }
+    }
+
+    public class VideoAccessModel
+    {
+        public string fullname { get; set; }
+        public int age { get; set; }
+    }
+
+    public class GroupModel
+    {
+        public string fullname { get; set; }
+        public string Subject { get; set; }
+        public string group { get; set; }
+        public List<Student> students { get; set; }
+    }
+
+    public class MyTemplateMethods
+    {
+        public static string GreetingMessage(string name)
+        {
+            return $"Здравствуйте, {name}! Вы уволены";
+        }
+
+        public static string DeliveryMessage(DeliveryModel model)
+        {
+            return $"Здравствуйте, {model.fullname}, проживающий по адресу {model.adress}, Ваша посылка дошла в пункт выдачи";
+        }
+
+        public static string VideoAccessMessage(VideoAccessModel model)
+        {
+            return $"Здравствуйте, {model.fullname}! {(model.age >= 18 ? "Вам стали доступны home video" : "Вот Ваша ссылка на Смешариков: https://www.youtube.com/playlist?list=PLeVA7eICJ6d1vzYh_SsxifcybiksP45D1")}";
+        }
+
+        public static string GroupInfoMessage(GroupModel model)
+        {
+            string message = $"Здравствуйте, {model.fullname}. Ваше название предмета: {model.Subject}; Номер группы: {model.group}; \nСписок группы: ";
+
+            foreach (var student in model.students)
+            {
+                message += $"\n-{student.Surname} {student.Name} ";
+            }
+
+            return message;
+        }
+    }
+}
